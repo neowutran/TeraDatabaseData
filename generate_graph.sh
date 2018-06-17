5bea05864ffbb5f364ba7e946c159b92693310ac
Neowutran <me@neowutran.ovh>
2018-06-17 12:11:38 +0200

    gnuplot

---
+++ generate_graph.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# new field separator, the end of line
+IFS=$'\n'
+
+for directory in $(find ./dps/ -type d 2> /dev/null) ; do
+   plotfiles=""
+   for file in $(ls $directory/*.txt 2> /dev/null); do
+      if [[ ! -z "$plotfiles" ]]; then
+         plotfiles="$plotfiles,"
+      fi
+
+      total_sum=$(cat $file | cut -d ":" -f 2 | paste -sd+ - | bc)
+      plotfiles="$plotfiles '$file' using (\$1*1e-3):(\$2*100/$total_sum) with linespoint"
+   done
+   if [[ ! -z "$plotfiles" ]]; then
+      gnuplot <<- EOF
+         set datafile separator ':'
+         set autoscale xy
+         set grid
+         set termoption enhanced
+         set format y '%1.0f%%'
+         set xtics 500
+         set terminal svg font "Bitstream Vera Sans, 12" size 2000,2000 linewidth 1
+         set output "|cat > $directory/plot.svg"
+         plot $plotfiles
+EOF
+   fi
+done
